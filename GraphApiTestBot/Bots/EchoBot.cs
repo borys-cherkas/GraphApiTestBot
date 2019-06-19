fb88befe877487add6288962845f93e97870f8e7
Borys Cherkas <borys.cherkas@outlook.com>
2019-06-20 00:35:38 +0300

    Intermediary changes with regards to MS Teams intergration.

--- GraphApiTestBot/Bots/EchoBot.cs
+++ GraphApiTestBot/Bots/EchoBot.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector.Teams;
 using Microsoft.Bot.Schema;
 
 namespace GraphApiTestBot.Bots
@@ -16,6 +18,28 @@ namespace GraphApiTestBot.Bots
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             await turnContext.SendActivityAsync(MessageFactory.Text($"Borys CD Echo: {turnContext.Activity.Text}"), cancellationToken);
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            var teamsContext = turnContext.TurnState.Get<ITeamsContext>();
+
+            if (teamsContext == null) return;
+
+            // Now fetch the Team ID, Channel ID, and Tenant ID off of the incoming activity
+            var incomingTeamId = teamsContext.Team.Id;
+            var incomingChannelid = teamsContext.Channel.Id;
+            var incomingTenantId = teamsContext.Tenant.Id;
+
+            // Make an operation call to fetch the list of channels in the team, and print count of channels.
+            var channels = await teamsContext.Operations.FetchChannelListAsync(incomingTeamId);
+            await turnContext.SendActivityAsync($"You have {channels.Conversations.Count} channels in this team");
+
+            // Make an operation call to fetch details of the team where the activity was posted, and print it.
+            var teamInfo = await teamsContext.Operations.FetchTeamDetailsAsync(incomingTeamId);
+            await turnContext.SendActivityAsync($"Name of this team is {teamInfo.Name} and group-id is {teamInfo.AadGroupId}");
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
